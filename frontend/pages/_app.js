400ed667b502286081b0cd52bb8eca1f800c8850
Grace Lee <jaeeungracelee@gmail.com>
2024-09-14 22:20:17 -0400

    added auth with google as an option

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,33 @@
-import '../styles/globals.css'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import '@/styles/globals.css'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ClerkProvider>
+      {/* signedout: show SignInButton when user is not signed in */}
+      <SignedOut>
+        <div className="flex justify-center items-center h-screen">
+          <SignInButton mode="modal" />
+        </div>
+      </SignedOut>
+
+      {/* signedin: show UserButton when user is signed in */}
+      <SignedIn>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <div className="fixed top-4 right-4">
+          <UserButton />
+        </div>
+      </SignedIn>
+    </ClerkProvider>
   )
 }
 
